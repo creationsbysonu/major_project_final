72f921c176304c5be20f59ef65d03aade1706775
NIGAM YADAV <71872897+nigamyadav72@users.noreply.github.com>
2025-07-13 17:35:58 +0545

    product and cart app added

--- majorproject/urls.py
+++ majorproject/urls.py
@@ -4,5 +4,6 @@ from django.urls import path , include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/',include('api.urls'))
+    path('products/',include('products.urls')),
+    path('cart/',include('cart.urls')),
 ]
---
+++ product/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+
+
+
+
+class ProductView(APIView):
+    pass
